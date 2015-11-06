210b7916f3866b49991e3b800868e99dc6e6c153
gongpuguo <904582819@163.com>
2015-11-06 23:58:05 +0800

    format code style

--- src/main/java/com/lawkingchina/app/springmvc/App.java
+++ src/main/java/com/lawkingchina/app/springmvc/App.java
@@ -2,12 +2,10 @@ package com.lawkingchina.app.springmvc;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
